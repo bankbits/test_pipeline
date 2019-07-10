bb16fcac5041816827db45088be820d7936ee5aa
dianabank <dianabank@ibm.com>
2019-07-10 13:47:23 -0400

    new groovy edit

--- readfiles.groovy
+++ readfiles.groovy
@@ -1,19 +1,62 @@
-// returns a list of changed files
-@NonCPS
-String getChangedFilesList() {
+import com.tikal.jenkins.plugins.multijob.*
+import hudson.*
+import hudson.model.*
+import hudson.plugins.git.*
+import hudson.slaves.*
+import hudson.tasks.*
 
-    def changeLogSets = currentBuild.rawBuild.changeSets
-    for (int i = 0; i < changeLogSets.size(); i++) {
-        def entries = changeLogSets[i].items
-        for (int j = 0; j < entries.length; j++) {
-            def entry = entries[j]
-            echo "${entry.commitId} by ${entry.author} on ${new Date(entry.timestamp)}: ${entry.msg}"
-            def files = new ArrayList(entry.affectedFiles)
-            for (int k = 0; k < files.size(); k++) {
-                def file = files[k]
-                echo "  ${file.editType.name} ${file.path}"
+def ln = System.getProperty('line.separator')
+println "---------------Groovy Changelog script Started---------------$ln"
+
+def lastSuccesfulBuild = build.previousNotFailedBuild
+def failed = build.result != hudson.model.Result.SUCCESS
+
+println "Last Succesful Build: ${lastSuccesfulBuild}"
+println "Current Build Result, is failed?: ${failed}"
+
+
+def currResult = build.result
+def prevResult = build.previousBuild?.result ?: null
+
+def consecutiveSuccess = currResult == hudson.model.Result.SUCCESS && prevResult == hudson.model.Result.SUCCESS
+
+def builds = []
+def changes = []
+def count = 0
+
+def getChangedFilesList() {
+
+    if (consecutiveSuccess) {
+        println "Last Build was sucessful, getting latest changes$ln"
+
+        builds << build
+        def changeItems = build.changeSet.items
+        println "Change Items: ${changeItems}$ln"
+
+        count += changeItems.length
+        changes += changeItems as List
+    } else {
+        println "Last Build was not sucessful, getting changes from all failed build as well$ln"
+
+        println "BUILD: $build$ln"
+
+        println "Hudson version: $build.hudsonVersion$ln"
+
+        println "Change set: $build.changeSet$ln"
+
+        println "Change set items: $build.changeSet.items$ln"
+
+        while (lastSuccesfulBuild) {
+            builds << lastSuccesfulBuild
+            def changeSet = lastSuccesfulBuild.changeSet
+            if (!changeSet.emptySet) {
+                def changeItems = lastSuccesfulBuild.changeSet.items
+                count += changeItems.length
+                changes += changeItems as List
             }
+            lastSuccesfulBuild = lastSuccesfulBuild.nextBuild
         }
     }
 }
+
 return this
