c241ce589d19e24271a2c3ce53652dfcddc8cd9d
dianabank <dianabank@ibm.com>
2019-07-10 13:39:33 -0400

    new groovy script

--- readfiles.groovy
+++ readfiles.groovy
@@ -2,16 +2,18 @@
 @NonCPS
 String getChangedFilesList() {
 
-    changedFiles = []
-    for (changeLogSet in currentBuild.changeSets) { 
-        for (entry in changeLogSet.getItems()) { // for each commit in the detected changes
-            for (file in entry.getAffectedFiles()) {
-                changedFiles.add(file.getPath()) // add changed file to list
+    def changeLogSets = currentBuild.rawBuild.changeSets
+    for (int i = 0; i < changeLogSets.size(); i++) {
+        def entries = changeLogSets[i].items
+        for (int j = 0; j < entries.length; j++) {
+            def entry = entries[j]
+            echo "${entry.commitId} by ${entry.author} on ${new Date(entry.timestamp)}: ${entry.msg}"
+            def files = new ArrayList(entry.affectedFiles)
+            for (int k = 0; k < files.size(); k++) {
+                def file = files[k]
+                echo "  ${file.editType.name} ${file.path}"
             }
         }
     }
-
-    return changedFiles
-
 }
 return this
